71d59d601e00f54542dda92aec0306fd47e094a8
Windows10 <youngcube@hotmail.com>
2022-02-16 12:15:22 +0800

    storybook --no-manager-cache

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const sassGlobals = [
-  path.join(__dirname, '../src/_sass_shared/_theme.scss')
+  path.join(__dirname, '../src/_sass_shared/_theme.scss').replace(/\\/g, '/')
 ]
 module.exports = {
   addons: [
@@ -47,7 +47,7 @@ module.exports = {
       test: /\.html$/,
       use: 'raw-loader',
     });
-    config.resolve.alias['@'] = path.join(__dirname, '../src')
+    config.resolve.alias['@'] = path.join(__dirname, '../src').replace(/\\/g, '/')
     return config
   },
 }
